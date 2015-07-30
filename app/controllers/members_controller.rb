5a549a4746a51372c95cfe1956ad6c1db136dc45
aditi73 <a.thakur73.at@gmail.com>
2015-07-30 13:52:05 +0530

    form validations done

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -14,7 +14,7 @@ class MembersController < ApplicationController
   # GET /members/1.json
   def show
     @member = Member.find(params[:id])
-
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @member }
