288a9dd7ea7c3916e4138516c19e8d55ba91c712
kmaheshwari <maheshwari.kajol@gmail.com>
2015-07-31 15:43:45 +0530

    foreign keys added

--- db/migrate/20150724073958_create_competetion_levels.rb
+++ db/migrate/20150724073958_create_competetion_levels.rb
@@ -2,7 +2,7 @@ class CreateCompetetionLevels < ActiveRecord::Migration
   def self.up
     create_table :competetion_levels do |t|
       t.string :age_group
-      
+      add_foreign_key(:members, :competetion_levels)
       t.timestamps
     end
   end
