52a5e70efe5b54e96dc3785ae99ca01eafa1aa33
aditi73 <a.thakur73.at@gmail.com>
2015-08-24 17:44:12 +0530

    welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 TaranPushkar3::Application.routes.draw do
 
- 
+ root :to => 'welcome#index'
   resources :grp_event_winners
 
 
@@ -73,7 +73,7 @@ TaranPushkar3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   
 
   # See how all your routes lay out with "rake routes"
 
